5aa2182f08bc1c20bc3882a544bd74fb8407e22b
Zawar-locofy <>
2023-05-08 13:58:39 +0000

    Updates

---
+++ _next/static/JhEt___kYkOI64_158vFa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/889-69ca91607e7d6ea7.js","static/css/65c238bf18ccceb3.css","static/chunks/pages/index-f11335145b530e39.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
